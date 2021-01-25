6a0daef684e9546dd9bf01e39e120d0f8a221221
Paramount22 <jurica.marek@gmail.com>
2021-01-25 22:02:35 +0100

    add js to header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,37 @@
-console.log('Hello');
+// selectors
+const header = document.querySelector('header');
+const cards = document.querySelectorAll('.card');
+
+// functions
+const changeBackground = (index) => {
+  if (index == 0) {
+    header.style.backgroundImage = "url('../assets/images/landscape.jpg')";
+  }
+
+  if (index == 1) {
+    header.style.backgroundImage =
+      "url('../assets/images/pexels-photo-261187.jpeg')";
+  }
+
+  if (index == 2) {
+    header.style.backgroundImage =
+      "url('../assets/images/pexels-photo-27411.jpg')";
+  }
+
+  if (index == 3) {
+    header.style.backgroundImage =
+      "url('../assets/images/food-salad-healthy-lunch.jpg')";
+  }
+
+  if (index == 4) {
+    header.style.backgroundImage =
+      "url('../assets/images/pexels-photo-210019.jpeg')";
+  }
+};
+
+// listeners
+cards.forEach((card, index) => {
+  card.addEventListener('mouseover', () => {
+    changeBackground(index);
+  });
+});
