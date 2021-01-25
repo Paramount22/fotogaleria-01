dc76f739954b01cf4be788c532efceefa39b904b
Paramount22 <jurica.marek@gmail.com>
2021-01-25 22:43:33 +0100

    animations

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,25 +5,30 @@ const cards = document.querySelectorAll('.card');
 // functions
 const changeBackground = (index) => {
   if (index == 0) {
+    header.classList.add('changeBackground');
     header.style.backgroundImage = "url('../assets/images/landscape.jpg')";
   }
 
   if (index == 1) {
+    header.classList.add('changeBackground');
     header.style.backgroundImage =
       "url('../assets/images/pexels-photo-261187.jpeg')";
   }
 
   if (index == 2) {
+    header.classList.add('changeBackground');
     header.style.backgroundImage =
       "url('../assets/images/pexels-photo-27411.jpg')";
   }
 
   if (index == 3) {
+    header.classList.add('changeBackground');
     header.style.backgroundImage =
       "url('../assets/images/food-salad-healthy-lunch.jpg')";
   }
 
   if (index == 4) {
+    header.classList.add('changeBackground');
     header.style.backgroundImage =
       "url('../assets/images/pexels-photo-210019.jpeg')";
   }
@@ -33,5 +38,9 @@ const changeBackground = (index) => {
 cards.forEach((card, index) => {
   card.addEventListener('mouseover', () => {
     changeBackground(index);
+  });
+
+  card.addEventListener('mouseleave', () => {
+    header.classList.remove('changeBackground');
   });
 });
